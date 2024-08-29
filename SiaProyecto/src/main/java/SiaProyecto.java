322b29ef632a4659e553883f6de3223bd0384898
Marti <martina.sandoval.o@mail.pucv.cl>
2024-08-28 22:49:39 -0400

    agregar clases...

---
+++ SiaProyecto/src/main/java/SiaProyecto.java
@@ -0,0 +1,8 @@
+
+public class SiaProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+                                                                                       
+    }
+}
